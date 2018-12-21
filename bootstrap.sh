93453b70fa6e570092f7d360d0a4354a60c4c2d9
Dilshod Mukhtarov <dilshodm@gmail.com>
2018-12-21 22:01:51 +0400

    bootstrap.sh: for MacOSX the aclocal directory changed from /opt/... to /usr/...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(uname)" == "Darwin" ]; then
-	include=" -I/opt/local/share/aclocal"
+	include=" -I/usr/local/share/aclocal"
 fi
 
 autoreconf --install --verbose --force $include
